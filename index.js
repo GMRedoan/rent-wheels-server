e114b14c4434584478af32808e818f107210a83d
GMRedoan <redoangazi69@gmail.com>
2025-11-28 19:26:03 +0600

    added new cars api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsw2asc.mongodb.net/?appName=Cluster0`;
- 
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -29,30 +29,33 @@ async function run() {
 
         const dataBase = client.db('rentWheels_db')
         const carsCollection = dataBase.collection('cars')
+        const newCarCollection = dataBase.collection('newCars')
+        const usersCollection = dataBase.collection('users')
 
-        app.get('/cars', async(req, res) => {
+        // all cars api
+        app.get('/cars', async (req, res) => {
             const cursor = carsCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
-        app.get('/cars/:id', async(req, res) => {
+        app.get('/cars/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await carsCollection.findOne(query)
             res.send(result)
         })
 
-        app.post('/cars', async(req, res) => {
+        app.post('/cars', async (req, res) => {
             const newCar = req.body
             const result = await carsCollection.insertOne(newCar)
             res.send(result)
         })
 
-        app.patch('/cars/:id', async(req, res) => {
+        app.patch('/cars/:id', async (req, res) => {
             const id = req.params.id
             const updatedCar = req.body
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const update = {
                 $set: {
                     car: updatedCar.car,
@@ -63,12 +66,41 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/cars/:id', async(req, res) => {
+        app.delete('/cars/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await carsCollection.deleteOne(query)
             res.send(result)
         })
+
+        // new cars api
+        app.get('/newCars', async (req, res) => {
+            const cursor = newCarCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // users api
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body
+            const email = req.body.email
+            const query = {email:email}
+            const existingUser = await usersCollection.findOne(query)
+            if(existingUser){
+                res.send({message:'already added this user in database'})
+            }
+            else {
+            const result = await usersCollection.insertOne(newUser)
+            res.send(result)
+            }
+        })
+
 
 
         await client.db("admin").command({ ping: 1 });
