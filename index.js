6322decf1c8b937f38647255688e19ecad8f1472
GMRedoan <redoangazi69@gmail.com>
2025-12-03 11:05:37 +0600

    bug fixes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 async function run() {
     try {
-        await client.connect()
+        // await client.connect()
 
         const dataBase = client.db('rentWheels_db')
         const carsCollection = dataBase.collection('cars')
